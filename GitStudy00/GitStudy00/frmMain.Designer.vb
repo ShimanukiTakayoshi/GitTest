fb625fab436347345a70769a6db06770e11cb4c4
shimanuki takayoshi <t-shima@db3.so-net.ne.jp>
2016-06-12 14:08:54 +0900

    ランプに枠をつけた

--- GitStudy00/GitStudy00/frmMain.Designer.vb
+++ GitStudy00/GitStudy00/frmMain.Designer.vb
@@ -39,11 +39,12 @@ Partial Class frmGitStudy
         'lblRump
         '
         Me.lblRump.AutoSize = True
+        Me.lblRump.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.lblRump.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.lblRump.Font = New System.Drawing.Font("ＭＳ ゴシック", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
         Me.lblRump.Location = New System.Drawing.Point(68, 144)
         Me.lblRump.Name = "lblRump"
-        Me.lblRump.Size = New System.Drawing.Size(134, 37)
+        Me.lblRump.Size = New System.Drawing.Size(136, 39)
         Me.lblRump.TabIndex = 1
         Me.lblRump.Text = "ランプ"
         '
